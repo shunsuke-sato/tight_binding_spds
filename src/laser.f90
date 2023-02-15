79e7d4a28c0b8fdba80a34e751d6dbfb1f3cc0d9
Shunsuke A. Sato <shunsuke.sato26@gmail.com>
2023-02-15 16:30:08 +0900

    Added the second laser.

--- src/laser.f90
+++ src/laser.f90
@@ -16,6 +16,13 @@ module laser
   real(8) :: pulse_center_1, time_delay_1
   real(8) :: omega_1, phi_CEP_1_x, phi_CEP_1_y, phi_CEP_1_z
 
+! laser 2
+  logical :: if_pulse_2
+
+  real(8) :: E0_pulse_2_x, E0_pulse_2_y, E0_pulse_2_z, pulse_width_2
+  real(8) :: pulse_center_2, time_delay_2
+  real(8) :: omega_2, phi_CEP_2_x, phi_CEP_2_y, phi_CEP_2_z
+
   
   contains
 !-------------------------------------------------------------------------------
@@ -25,6 +32,11 @@ module laser
       real(8) :: E0_pulse_1_x_Vm, E0_pulse_1_y_Vm, E0_pulse_1_z_Vm, pulse_width_1_fs
       real(8) :: pulse_center_1_fs, time_delay_1_fs
       real(8) :: omega_1_ev, phi_CEP_1_x_2pi, phi_CEP_1_y_2pi, phi_CEP_1_z_2pi
+! laser 2
+      real(8) :: E0_pulse_2_x_Vm, E0_pulse_2_y_Vm, E0_pulse_2_z_Vm, pulse_width_2_fs
+      real(8) :: pulse_center_2_fs, time_delay_2_fs
+      real(8) :: omega_2_ev, phi_CEP_2_x_2pi, phi_CEP_2_y_2pi, phi_CEP_2_z_2pi
+
 
 
       
@@ -60,6 +72,38 @@ module laser
       if_pulse_1 = .true.
       if(abs(E0_pulse_1_x) + abs(E0_pulse_1_y) + abs(E0_pulse_1_z) == 0) if_pulse_1 = .false.
 
+! laser 2
+      call read_basic_input('E0_pulse_2_x_Vm',E0_pulse_2_x_Vm,val_default = 0d0)
+      call read_basic_input('E0_pulse_2_y_Vm',E0_pulse_2_y_Vm,val_default = 0d0)
+      call read_basic_input('E0_pulse_2_z_Vm',E0_pulse_2_z_Vm,val_default = 0d0)
+
+      call read_basic_input('pulse_width_2_fs',pulse_width_2_fs,val_default = 0d0)
+      call read_basic_input('pulse_center_2_fs',pulse_center_2_fs &
+                             ,val_default = pulse_width_2_fs*0.5d0)
+      call read_basic_input('time_delay_2_fs',time_delay_2_fs,val_default = 0d0)
+
+      call read_basic_input('omega_2_ev',omega_2_ev,val_default = 1d0)
+
+      call read_basic_input('phi_CEP_2_x_2pi',phi_CEP_2_x_2pi,val_default = 0d0)
+      call read_basic_input('phi_CEP_2_y_2pi',phi_CEP_2_y_2pi,val_default = 0d0)
+      call read_basic_input('phi_CEP_2_z_2pi',phi_CEP_2_z_2pi,val_default = 0d0)
+
+
+      E0_pulse_2_x = E0_pulse_2_x_Vm*ev/(angstrom*1d10)
+      E0_pulse_2_y = E0_pulse_2_y_Vm*ev/(angstrom*1d10)
+      E0_pulse_2_z = E0_pulse_2_z_Vm*ev/(angstrom*1d10)
+
+      pulse_width_2 = pulse_width_2_fs*fs
+      pulse_center_2 = pulse_center_2_fs*fs
+      time_delay_2 = time_delay_2_fs*fs
+      omega_2        = omega_2_ev*ev
+      phi_CEP_2_x    = phi_CEP_2_x_2pi*2d0*pi 
+      phi_CEP_2_y    = phi_CEP_2_y_2pi*2d0*pi 
+      phi_CEP_2_z    = phi_CEP_2_z_2pi*2d0*pi 
+
+      if_pulse_2 = .true.
+      if(abs(E0_pulse_2_x) + abs(E0_pulse_2_y) + abs(E0_pulse_2_z) == 0) if_pulse_2 = .false.
+
     end subroutine init_laser
 !-------------------------------------------------------------------------------
     subroutine calc_vector_potential_time(tt, Act)
@@ -83,6 +127,24 @@ module laser
 
           Act(3) = Act(3) -(E0_pulse_1_z/omega_1)*sin(omega_1*xx+phi_CEP_1_z) &
             *cos(pi*xx/pulse_width_1)**4
+
+        end if
+      end if
+
+
+! laser 2
+      if(if_pulse_2)then
+        xx = tt-pulse_center_2 - time_delay_2
+        if(abs(xx)<0.5d0*pulse_width_2)then
+
+          Act(1) = Act(1) -(E0_pulse_2_x/omega_2)*sin(omega_2*xx+phi_CEP_2_x) &
+            *cos(pi*xx/pulse_width_2)**4
+
+          Act(2) = Act(2) -(E0_pulse_2_y/omega_2)*sin(omega_2*xx+phi_CEP_2_y) &
+            *cos(pi*xx/pulse_width_2)**4
+
+          Act(3) = Act(3) -(E0_pulse_2_z/omega_2)*sin(omega_2*xx+phi_CEP_2_z) &
+            *cos(pi*xx/pulse_width_2)**4
 
         end if
       end if
